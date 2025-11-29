d6fcf3bcf2952402d2a96497dd4c71488696d376
SandipCodes <sandipsh321@gmail.com>
2025-11-30 01:09:36 +0530

    another commit with new usecases..

--- CollectionDemo/src/cpm/bainbeast/poc/StreamApiTest.java
+++ CollectionDemo/src/cpm/bainbeast/poc/StreamApiTest.java
@@ -9,18 +9,38 @@ public class StreamApiTest {
 		
 		List<String> names=Arrays.asList("Sandip","Sachin","Sunita","Archana","Ashiwini");
 		
+		List<String> contacts=Arrays.asList("7709595657","9881365999",
+				                            "9518386334","9561605305",
+				                            "7709595657","9881365999");
+		
+		
+				     
+				
+				          
 		
 		names.stream()
 	    .forEach(System.out :: println);
 
+		System.out.println("-----------------------------");
 		names.stream()
 		    .map(String :: toUpperCase)
 		    .forEach(System.out :: println);
 
+		System.out.println("-----------------------------");
 		names.stream()
 	    .map(String :: toLowerCase)
 	    .forEach(System.out :: println);
 
+		System.out.println("-----------------------------");
+		//remove duplicate from contacts.
+		contacts.stream()
+        .forEach(System.out :: println);
+         
+		System.out.println("-----------------------------");
+		contacts.stream()
+		        .distinct()
+		        .forEach(System.out :: println);
+		System.out.println("-----------------------------");
 	}//main()
 
 }//class
