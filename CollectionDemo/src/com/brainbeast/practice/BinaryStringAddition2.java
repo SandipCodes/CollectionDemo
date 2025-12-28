3580d3db7973a1503d92ae43badf910591c3fb1d
SandipCodes <sandipsh321@gmail.com>
2025-12-29 01:11:42 +0530

    BinaryStringAddition using two pointers

---
+++ CollectionDemo/src/com/brainbeast/practice/BinaryStringAddition2.java
@@ -0,0 +1,59 @@
+package com.brainbeast.practice;
+
+//addition using two pointers
+
+public class BinaryStringAddition2 {
+
+	
+	private static String add_binary(String x, String y) {
+		
+		 int i=x.length() -1;
+		 int j=y.length() -1;
+		 
+		 int carry=0;
+		 
+		 StringBuilder result=new StringBuilder();
+		 
+		 while(i>=0 || j>=0) {
+			 
+			 int sum=carry;
+			 
+			 if(i >= 0) {
+				 sum += x.charAt(i) - '0';
+			 }
+			 if(j >= 0) {
+				 sum += y.charAt(j) - '0';
+			 }
+			 
+			 if(sum==0 || sum==1) {
+				 result.append(sum);
+				 carry=0;
+			 }
+			 else if(sum == 2) {
+				 result.append('0');
+				 carry=1;
+			 }
+			 else {
+				 result.append('1');
+				 carry=1;
+			 }
+			 i--;
+			 j--;
+		 }
+		 if(carry==1) {
+			 result.append('1');
+		 }
+		 
+		return result.reverse().toString();
+	}
+	
+	public static void main(String[] args) {
+		String x="1001";
+		String y="0110";
+		
+		System.out.print(add_binary(x,y));
+	}
+
+	
+
+}
