e629331f059adbbb8db3882d64ec3ceef803424c
SandipCodes <sandipsh321@gmail.com>
2025-12-29 01:58:13 +0530

    added OddEven checker for Numbers using git commands

---
+++ CollectionDemo/src/com/brainbeast/practice/OddEvenTest.java
@@ -0,0 +1,87 @@
+package com.brainbeast.practice;
+
+import java.util.Scanner;
+
+
+//using brtforce method /2
+public class OddEvenTest {
+
+	public static void main(String[] args) {
+		
+		int num=0;
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the number:");
+		num=sc.nextInt();
+		checkNum1(num);
+		System.out.println();
+
+		System.out.println("Enter the number:");
+		num=sc.nextInt();
+		checkNum2(num);
+		System.out.println();
+		
+		System.out.println("Enter the number:");
+		num=sc.nextInt();
+		checkNum3(num);
+		System.out.println();
+		
+		System.out.println("Enter the number:");
+		num=sc.nextInt();
+		checkNum4(num);
+		System.out.println();
+
+	}
+    
+	//Using Bitwise XOR
+	private static void checkNum4(int num) {
+		
+		if((num ^ 1) == num+1) {
+			System.out.println("Even Number");
+		}
+		else {
+			System.out.println("Odd Number");
+		}
+		
+	}
+
+	//// Using Bitwise AND
+	 private static void checkNum3(int num) {
+		
+		 
+		 if((num & 1) == 1) {
+			 System.out.println("Odd Number");
+		 }
+		 else {
+			 System.out.println("Even Number.");
+		 }
+		 
+		
+	}
+
+	 //Using Bitwise OR
+     private static void checkNum2(int num) {
+    	
+		System.out.println("OddEvenTest.checkNum2()");
+		if((num | 1) > num) {
+			System.out.println("number is Even");
+		}
+		else {
+			System.out.println("number is odd");
+		}
+	}
+
+	 // divide by 2
+	private static void checkNum1(int num) {
+		
+		System.out.println("OddEvenTest.checkNum1()");
+		if(num % 2==0) {
+			System.out.println("No is Even");
+		}
+		else {
+			System.out.println("No is Odd");
+		}
+	}
+
+}
